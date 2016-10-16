8ba0e01e468bee373dfa8500aca2f8dd8a410664
veksarev <vae96@live.com>
2016-10-16 11:01:17 -0700

    Updated reps

--- AccelerometerApp/app/src/main/java/com/spencerpeters/accelerometerapp/RepsCalculator.java
+++ AccelerometerApp/app/src/main/java/com/spencerpeters/accelerometerapp/RepsCalculator.java
@@ -8,15 +8,25 @@ import java.util.Iterator;
 
 public class RepsCalculator {
     private static final int AXIS = 1;
-    private static final float margin = 0.5f;
-
+    private static final float margin = 1.0f;
 
     public static int numPeaks(Iterator<float[]> it){
         int peaks = 0;
-        float prev = 0.0f;
+        float prevMinMax = 0.0f;
         while(it.hasNext()){
-            if(!almostZero(prev) && almostZero(it.next()[AXIS])){
+            float current = it.next()[AXIS];
+            if((prevMinMax > margin && current < (0.0 - margin))||(prevMinMax < (0.0 - margin) && current > margin)){
                 peaks++;
+                prevMinMax = current;
+            }
+            if(prevMinMax > 0.0f){
+                if(current > prevMinMax){
+                    prevMinMax = current;
+                }
+            } else {
+                if(current < prevMinMax){
+                    prevMinMax = current;
+                }
             }
         }
         return peaks;

fb491997a12f60eca5483c9a2f5c04cf2db24207
veksarev <vae96@live.com>
2016-10-16 15:19:25 -0700

    Button

--- AccelerometerApp/app/src/main/java/com/spencerpeters/accelerometerapp/MainActivity.java
+++ AccelerometerApp/app/src/main/java/com/spencerpeters/accelerometerapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.spencerpeters.accelerometerapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.*;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Arrays;
 
@@ -13,12 +16,42 @@ import java.util.Arrays;
 // Hi this is a commit
 public class MainActivity extends AppCompatActivity {
 
+    public Intent intent;
     public RotationalData data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, DynamicXYPlotActivity.class);
-        startActivity(intent);
+        intent = new Intent(this, DynamicXYPlotActivity.class);
     }
+
+    public class StartButton extends Activity {
+        protected void onCreate(Bundle icicle) {
+            super.onCreate(icicle);
+
+            setContentView(R.layout.content_layout_id);
+
+            final Button button = (Button) findViewById(R.id.button_id);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+ /*   public class EndButton extends Activity {
+        protected void onCreate(Bundle icicle) {
+            super.onCreate(icicle);
+
+            setContentView(R.layout.content_layout_id);
+
+            final Button button = (Button) findViewById(R.id.button_id);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }*/
 }
